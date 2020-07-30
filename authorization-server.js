e201f96f257bb0e614ed714c0dff103e27f976c6
Luis Santos <luis.eugenio.santos@accenture.com>
2020-07-30 12:10:00 -0500

    Adding authentication get

--- authorization-server.js
+++ authorization-server.js
@@ -53,6 +53,9 @@ app.use(bodyParser.urlencoded({ extended: true }))
 /*
 Your code here
 */
+app.get('/authorize', (req, res ) => {
+	res.end(200)
+})
 
 const server = app.listen(config.port, "localhost", function () {
 	var host = server.address().address
